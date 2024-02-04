767bdd5255cb116ce0868b6ea150fbce7fa79d95
Victor-Lis <victorlisbronzo1@gmail.com>
2024-02-03 21:40:54 -0300

    v1.09
    - Adicionando relógio digital para facilitar a interpretação

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,14 @@
 const ponteiro = document.querySelector("#ponteiro")
+const time = document.querySelector("#time")
 
 function getHours(){
     let hours = new Date().getHours()   
     let min = new Date().getMinutes()
+    time.innerText = `${formatNumber(hours)}:${formatNumber(min)}`
     ponteiro.style.transform = `rotate(${(hours+(min/60))*30}deg)`
 }
+
+const formatNumber = (n) => n < 10? "0"+n : n
 
 setInterval(() => {
     getHours()
